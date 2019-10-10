44872a76692b57bd5ab223998e14fc326bbbeee8
KRISHNA SONI <mac-0005@example.com>
2019-10-10 10:39:59 +0530

    - Initial commit

---
+++ Lassi-ios/Lassi_ios.h
@@ -0,0 +1,19 @@
+//
+//  Lassi_ios.h
+//  Lassi-ios
+//
+//  Created by mac-0005 on 10/10/19.
+//  Copyright © 2019 mac-0005. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Lassi_ios.
+FOUNDATION_EXPORT double Lassi_iosVersionNumber;
+
+//! Project version string for Lassi_ios.
+FOUNDATION_EXPORT const unsigned char Lassi_iosVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Lassi_ios/PublicHeader.h>
+
+
---
+++ Lassi-ios/RandomString.swift
@@ -0,0 +1,16 @@
+//
+//  RandomString.swift
+//  Lassi-ios
+//
+//  Created by mac-0005 on 10/10/19.
+//  Copyright © 2019 mac-0005. All rights reserved.
+//
+
+import Foundation
+
+open class RandomString: NSObject {
+    
+    open func getRandomString() -> String {
+        return ""
+    }
+}
